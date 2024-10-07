15dbff48b608515ab254684bbd6caddcf2184163
uriii3 <oriolrv689@gmail.com>
2024-10-07 11:38:20 +0200

    finalising the dictionary

--- example.py
+++ example.py
@@ -1,3 +1,10 @@
 import copernicusmarine as cm
 
 help(cm.subset)
+
+string = """{zero} hola {one} {one}, {zero}"""
+
+numbers_dict = {"one": "1", "two": "2", "zero": "3"}
+
+print(numbers_dict)
+print(string.format(**numbers_dict))
